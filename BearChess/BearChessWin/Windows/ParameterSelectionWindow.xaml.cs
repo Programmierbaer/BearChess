dbe6658ca485413f68f12c5d7459204da60138f5
Lars Nowak <lars.nowak@outlook.de>
2024-01-03 23:08:22 +0100

    Version 1.0.4.1

--- BearChess/BearChessWin/Windows/ParameterSelectionWindow.xaml.cs
+++ BearChess/BearChessWin/Windows/ParameterSelectionWindow.xaml.cs
@@ -132,7 +132,7 @@ namespace www.SoLaNoSoft.com.BearChessWin
 
         private void ListBoxEngines_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (SelectedEngines.Length>0)
+            if (SelectedEngines.Length>0 && e.OriginalSource is TextBlock)
             {
                 DialogResult = true;
             }
